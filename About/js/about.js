cd9bf3f802451d870fc2d5c95950f4e80113a14a
stefan döcker <stefandoecker@stefans-MacBook-Pro.local>
2018-01-24 20:13:18 +0100

    Version_1.0

--- About/js/about.js
+++ About/js/about.js
@@ -1,6 +1,6 @@
-$(".wir").mouseover(function(){
+$(".wir").mouseover(function () {
     $(this).width(180).height(280);
-    $(".wir").mouseleave(function(){
+    $(".wir").mouseleave(function () {
         $(this).width(120).height(200);
         $("#david").width(140);
     });
--- Shop/js/shop.js
+++ Shop/js/shop.js
@@ -1,6 +1,41 @@
-$("#zehn").click(function(){
+$("#myBtn").click(function () {
 
-   $("#rows").fadeOut("slow");
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Alles ist passt</h5>");
+
 })
 
-$()
+$("#myBtn1").click(function () {
+
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Produkt Abmeldung, Preis kostenlos (PAPK)</h5>");
+
+})
+
+$("#myBtn2").click(function () {
+
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Eat Sleep MSDN Repeat</h5>");
+
+})
+
+$("#myBtn3").click(function () {
+
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Mimimimimi</h5>");
+
+})
+
+$("#myBtn4").click(function () {
+
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Fetzen!</h5>");
+
+})
+
+$("#myBtn5").click(function () {
+
+    $("h5").remove();
+    $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Drop Table Fehlstunden</h5>");
+
+})
