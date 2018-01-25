a7fcfc151f2800daa13e041eb9a53a7031bb5eca
stefan döcker <stefandoecker@stefans-MacBook-Pro.local>
2018-01-25 22:36:54 +0100

    new version

--- Shop/js/shop.js
+++ Shop/js/shop.js
@@ -1,41 +1,51 @@
+
+$("#carouselExampleSlidesOnly").carousel({interval: 4000});
+
+
 $("#myBtn").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Alles ist passt</h5>");
-
+    $("#pic").html( "<img src='./bilder/tshirt.png' id='pics' style='width:200px;' alt=''>");
 })
 
 $("#myBtn1").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Produkt Abmeldung, Preis kostenlos (PAPK)</h5>");
-
+    $("#pic").html( "<img src='./bilder/Papk.png' id='pics' style='width:200px;' alt=''>");
 })
 
 $("#myBtn2").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Eat Sleep MSDN Repeat</h5>");
-
+    $("#pic").html( "<img src='./bilder/msdn.png' id='pics' style='width:200px;' alt=''>");
 })
 
 $("#myBtn3").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Mimimimimi</h5>");
-
+    $("#pic").html( "<img src='./bilder/mimimi.png' id='pics' style='width:200px;' alt=''>");
 })
 
 $("#myBtn4").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Fetzen!</h5>");
-
+    $("#pic").html( "<img src='./bilder/fetzen.png' id='pics' style='width:200px;' alt=''>");
 })
 
 $("#myBtn5").click(function () {
 
     $("h5").remove();
+    $("#pics").remove();
     $(".modal-header").html("<h5 class='modal-title' id='exampleModalLabel'>Drop Table Fehlstunden</h5>");
-
+    $("#pic").html( "<img src='./bilder/droptable.png' id='pics' style='width:200px;' alt=''>");
 })
